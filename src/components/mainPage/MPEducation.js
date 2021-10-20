9148bd2523c43e3008c00c8d9ee88edda598dc05
F-Agata <hello@agataf.pl>
2021-10-20 14:39:29 +0200

    wip MainPage

--- src/components/mainPage/MPEducation.js
+++ src/components/mainPage/MPEducation.js
@@ -22,21 +22,24 @@ const WrappMPEducation = styled.div`
 const TitleEducation = styled(TitleOfSection)`
   width: 100%;
   margin: 0 0 40px 0;
-  //border-bottom: 5px solid ${props => props. theme.colors.colorGrayLighter} ;
- `
+   `
 
 const WrappOneSchool = styled(WrappMPOneItem)` 
     position: relative;
 `
 
 const Yers = styled(SmallTitleMPOneItem)`
-  //border: #61dafb 2px solid;
-  margin-top:  ${props => props.mgrfarm ? '10px' : '0px'};
+
+//border: #61dafb 2px solid;
+margin-top:  ${props => props.mgrfarm ? '10px' : '0px'};
+border-bottom-width: ${props => props.mgrfarm ? '0px' : '1px'};
+font-size: ${props => props.mgrfarm ? props.theme.fontSizes.fsB3 : props.theme.fontSizes.fsB1 };
+  
    `
 
 const NameOfSchool = styled.p`
     margin: 10px 0 0 0 ;
-`
+ `
 const MPEducation = () => {
 
     const showDecorationLine = useMediaQuery({query: '(min-width: 1080px)' })
@@ -55,9 +58,9 @@ const MPEducation = () => {
                     <Circle/>
                     <DashFromTheCircle/>
                    <Yers> 2005 – 2011  </Yers>
-                   <Yers mgrfarm> mgr farmacji  </Yers>
                    <NameOfSchool> Śląski Uniwersytet Medyczny w Katowicach </NameOfSchool>
                    <NameOfSchool> Wydział Farmaceutyczny z Oddziałem Medycyny Laboratoryjnej w Sosnowcu</NameOfSchool>
+                   <Yers mgrfarm> mgr farmacji  </Yers>
                 </WrappOneSchool>
                 <WrappOneSchool>
                     <Circle/>
@@ -65,7 +68,7 @@ const MPEducation = () => {
                    <Yers> 2002 – 2005 </Yers>
                    <NameOfSchool> I Liceum Ogólnokształcące im. Leona Kruczkowskiego w Tychach</NameOfSchool>
                 </WrappOneSchool>
-            </WrappMPEssence>
+             </WrappMPEssence>
         </WrappMPEducation>
     )
 }
--- src/styles/SmallTitleMPOneItem.js
+++ src/styles/SmallTitleMPOneItem.js
@@ -5,6 +5,7 @@ const SmallTitleMPOneItem = styled.h4`
   font-size: ${props => props.theme.fontSizes.fsB1};
   font-weight: 400;
   color:  ${props => props.theme.colors.colorText};
+  border-bottom: 1px solid ${props => props.theme.colors.colorPrimary};
   `;
 
 export default SmallTitleMPOneItem;
