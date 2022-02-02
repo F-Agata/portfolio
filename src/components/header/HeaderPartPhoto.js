03f432b2b55ec58e0efa2563e29f28535f2fa9b6
F-Agata <hello@agataf.pl>
2022-02-02 11:50:45 +0100

    add react-spring animation to photo

--- src/components/header/HeaderPartPhoto.js
+++ src/components/header/HeaderPartPhoto.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import styled from 'styled-components'
+import { useSpring, animated } from 'react-spring'
 
 import agata from '../../pictures/agata.png'
 
+const calc = (x, y) => [-(y - window.innerHeight / 2) / 20, (x - window.innerWidth / 2) / 20, 1.1]
+const trans = (x, y, z) => `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${z})`
+
 const HeaderPartPhoto = () => {
+
+     const [spring, setSpring] = useSpring(()=>({ xyz: [0, 0, 1], config: {mass: 10, tension: 200, friction:50}}))
+
   return (
-    <WrappBigPhoto>
+    <WrappBigPhoto
+        onMouseMove={({ clientX: x, clientY: y}) => (setSpring({ xyz: calc(x, y)}))}
+        onMouseLeave={() => setSpring({ xyz: [0, 0, 1]})}
+        style={ {transform: spring.xyz.interpolate(trans)}}
+    >
       <ShadowDiv />
       <WrappSmallPhoto>
         <PhotoMy src={agata} alt={"I'm"} />
@@ -16,9 +27,11 @@ const HeaderPartPhoto = () => {
 
 export default HeaderPartPhoto
 
-const WrappBigPhoto = styled.div`
+const WrappBigPhoto = styled(animated.div)`
   max-width: 900px;
   position: relative;
+  //border: yellowgreen 2px solid;
+  cursor: pointer;
   `
 
 const WrappSmallPhoto = styled.div`
@@ -30,12 +43,14 @@ const WrappSmallPhoto = styled.div`
   left: 0px;
   right: 0;
   z-index: 1;
+  //border: purple 2px solid;
 `
 const PhotoMy = styled.img`
   width: 100%;
   `
 
 const ShadowDiv = styled.div`
+  //border: yellow 2px solid;
   max-width: 900px;
   position: absolute;
   bottom: 0;
